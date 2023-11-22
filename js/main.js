db105593c8549f1cc38c4e01d21d4043cf257a31
Simona Sud <ssimona.sud@gmail.com>
2023-11-22 10:59:51 +0200

    padarytos uzduotys

--- js/main.js
+++ js/main.js
@@ -20,3 +20,32 @@ const lyginiai = mainArr.filter((sk) => sk % 2 === 0);
 console.log("lyginiai ===", lyginiai);
 let sk = 21;
 console.log(sk % 2 === 0);
+
+// Visas neigiamas vertes masyve padaryti teigiamomis
+let onlyPositivesArr = mainArr.map((sk) => {
+  //kiekviena reiksme kaip sk
+  //tikrinti ar skaicius yra maziau uz 0
+  // if (sk < 0) {
+  //   let kint = sk * -1;
+  //   return kint;
+  // }
+  // return sk;
+  // sk < 0 ? return sk * -1 : return sk
+  return sk < 0 ? sk * -1 : sk;
+  // jei taip tai * -1
+  //grazinti reiksme kad griztu i onlyPositivesArr
+});
+onlyPositivesArr = mainArr.map((sk) => (sk < 0 ? sk * -1 : sk));
+//visiems is eiles pritaikome taisykle Math.abs
+onlyPositivesArr = mainArr.map((sk) => Math.abs(sk));
+console.log("onlyPositivesArr ===", onlyPositivesArr);
+
+// Atrinkti kas penktą elementą
+//panaudoti filtra
+//viduje tikrinam ar indexas yra 5 kartotinis(inx % 5 === 0)
+//jei yra tai atrenkam
+//jei ne praleidziam
+
+const atrinktiKasPenkta = mainArr.filter((sk, index) => index % 5 === 0);
+
+console.log(atrinktiKasPenkta);
