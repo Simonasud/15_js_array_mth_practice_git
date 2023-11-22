4e15e2e668593c3ee1e17995fe78ab7de09c6f81
Simona Sud <ssimona.sud@gmail.com>
2023-11-22 11:57:03 +0200

    suma

--- js/mixed.js
+++ js/mixed.js
@@ -16,3 +16,29 @@ const mixedArray = [
   "and",
   3,
 ];
+
+//sukurti funkcija, kuri ima masyva kaip argumenta
+//a. grazina visu jo skaiciu suma
+//b. grazina visu jo skaiciu vidurki
+
+/**
+ *
+ * @param {(string | number | boolean)[]} arr
+ */
+function calculateFromArr(arr) {
+  //psiaudo kodas
+  let suma = 0;
+  //sukti cikla per masyva
+  arr.forEach((elementas) => {
+    //patikrinti ar tipas yra sk
+    if (typeof elementas === "number") {
+      //jei sk tai sudeti su kazkuo
+      console.log(elementas);
+      suma += elementas;
+    }
+  });
+  console.log("suma ===", suma);
+  return suma;
+}
+let rez = calculateFromArr(mixedArray);
+console.log("rez ===", rez);
