6a0ab4a1593c52ca1ade89f1264ad11b4ebf5ac0
Simona Sud <ssimona.sud@gmail.com>
2023-11-22 12:06:16 +0200

    padaryta

--- js/mixed.js
+++ js/mixed.js
@@ -28,17 +28,37 @@ const mixedArray = [
 function calculateFromArr(arr) {
   //psiaudo kodas
   let suma = 0;
+  let kiekSkaiciu = 0;
   //sukti cikla per masyva
   arr.forEach((elementas) => {
     //patikrinti ar tipas yra sk
     if (typeof elementas === "number") {
       //jei sk tai sudeti su kazkuo
       console.log(elementas);
+      kiekSkaiciu++;
       suma += elementas;
     }
   });
   console.log("suma ===", suma);
+  console.log("kiekSkaiciu ===", kiekSkaiciu);
   return suma;
 }
+
 let rez = calculateFromArr(mixedArray);
 console.log("rez ===", rez);
+
+//atrinkti skaicius
+const skArr = mixedArray.filter((el) => typeof el === "number");
+console.log("skArr ===", skArr);
+
+let daug2 = skArr.filter((sk) => sk > 2);
+console.log("daug2 ===", daug2);
+
+let liArr = daug2.map((sk) => `<li>${sk} skaicius </li>`);
+console.log("liArr  ===", liArr);
+
+liArr = mixedArray
+  .filter((el) => typeof el === "number")
+  .filter((sk) => sk > 2)
+  .map((sk) => `<li>${sk} skaicius </li>`);
+console.log("liArr ===", liArr);
